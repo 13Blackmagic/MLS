917b5818fd6c769fa32cf3a2d790027230e86b56
13Blackmagic <123032739+13Blackmagic@users.noreply.github.com>
2023-12-12 14:14:01 -0700

    adding to mutations

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -15,7 +15,7 @@ const Header = () => {
           <Link className="text-light" to="/">
             <h1 className="m-0">National MLS</h1>
           </Link>
-          <p className="m-0">Let's all help eachother.</p>
+          <p className="m-0">By agents for agents.</p>
         </div>
         <div>
           {Auth.loggedIn() ? (
